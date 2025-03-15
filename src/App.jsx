ea8487978566f17cad14f677d01f512f56ce767a
Artem Shvachov <shvachev91@gmail.com>
2025-03-15 09:26:44 +0200

    add localstorage and useUffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 import Feedback from "./components/Feedback";
 import Options from "./components/Options";
@@ -6,19 +6,24 @@ import Description from "./components/Description";
 import Notification from "./components/Notification";
 
 function App() {
-  const [reviews, setReviews] = useState({
-    good: 0,
-    neutral: 0,
-    bad: 0,
+  const [reviews, setReviews] = useState(() => {
+    const savedReviews = window.localStorage.getItem("saved-reviews");
+    return savedReviews
+      ? JSON.parse(savedReviews)
+      : { good: 0, neutral: 0, bad: 0 };
   });
   const totalFeedback = reviews.good + reviews.neutral + reviews.bad;
 
   const updateFeedback = (feedbackType) => {
-    setReviews({
-      ...reviews,
-      [feedbackType]: reviews[feedbackType] + 1,
-    });
+    setReviews((prevReviews) => ({
+      ...prevReviews,
+      [feedbackType]: prevReviews[feedbackType] + 1,
+    }));
   };
+
+  useEffect(() => {
+    window.localStorage.setItem("saved-reviews", JSON.stringify(reviews));
+  }, [reviews]);
 
   const resetFeedback = () => {
     setReviews({ good: 0, neutral: 0, bad: 0 });
